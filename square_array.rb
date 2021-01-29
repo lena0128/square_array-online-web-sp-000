a7af4d87e047da37d939a42d884b9d419698b010
Lena Hou <lenahou2013@gmail.com>
2021-01-29 00:33:54 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   # your code here
   new_array = []
-  array.each do |element|
-    new.array.push(element ** 2)
+  array.each {|number| new_array.push(number ** 2) }
+    return new_array
  end
 end
